df8313161ea2b8e62caf7b233b4c432f44d85a20
Mariana Luduena <marianabelenluduena@gmail.com>
2022-12-01 10:25:52 -0300

    Función para crear elementos
    Se terminó de integrar a la función crearElementos el código para crear el cuerpo de la tarjeta, título, descripción y se integró todo al catálogo. Falta trabajar en la tag img para mostrar el src y el alt

--- js/index.js
+++ js/index.js
@@ -5,53 +5,53 @@
  */
 
 let productos = [{
-        id: 1,
-        nombre: 'Lápiz labial rojo',
-        descripcion: 'Lápiz labial color rojo',
-        precio: 2.000,
-        imagen: '../img/labial.png',
-        categoría: 'Boca',
-    },
-    {
-        id: 2,
-        nombre: 'Sombra de ojos',
-        descripcion: 'Varios colores de sombra de ojos',
-        precio: 3.200,
-        imagen: '../img/sombra__de__ojos.png',
-        categoría: 'Ojos',
-    },
-    {
-        id: 3,
-        nombre: 'Rubor rosado',
-        descripcion: 'Rubor color rosado',
-        precio: 1.500,
-        imagen: '../img/rubor.png',
-        categoría: 'Cara',
-    },
-    {
-        id: 4,
-        nombre: 'Delineador negro',
-        descripcion: 'Delineador color negro',
-        precio: 1.250,
-        imagen: '../img/delineador.png',
-        categoría: 'Ojos',
-    },
-    {
-        id: 5,
-        nombre: 'Crema corporal de castañas',
-        descripcion: 'Botella de crema corporal de castañas',
-        precio: 2.100,
-        imagen: '../img/crema__corporal.png',
-        categoría: 'Cuerpo',
-    },
-    {
-        id: 6,
-        nombre: 'Shampoo antiquiebre de castañas',
-        descripcion: 'Botellas de shampoo y acondicionador para el cabello',
-        precio: 2.550,
-        imagen: '../img/shampoo.png',
-        categoría: 'Cabello',
-    },
+    id: 1,
+    nombre: 'Lápiz labial rojo',
+    descripcion: 'Lápiz labial color rojo',
+    precio: 2.000,
+    imagen: '../img/labial.png',
+    categoría: 'Boca',
+},
+{
+    id: 2,
+    nombre: 'Sombra de ojos',
+    descripcion: 'Varios colores de sombra de ojos',
+    precio: 3.200,
+    imagen: '../img/sombra__de__ojos.png',
+    categoría: 'Ojos',
+},
+{
+    id: 3,
+    nombre: 'Rubor rosado',
+    descripcion: 'Rubor color rosado',
+    precio: 1.500,
+    imagen: '../img/rubor.png',
+    categoría: 'Cara',
+},
+{
+    id: 4,
+    nombre: 'Delineador negro',
+    descripcion: 'Delineador color negro',
+    precio: 1.250,
+    imagen: '../img/delineador.png',
+    categoría: 'Ojos',
+},
+{
+    id: 5,
+    nombre: 'Crema corporal de castañas',
+    descripcion: 'Botella de crema corporal de castañas',
+    precio: 2.100,
+    imagen: '../img/crema__corporal.png',
+    categoría: 'Cuerpo',
+},
+{
+    id: 6,
+    nombre: 'Shampoo antiquiebre de castañas',
+    descripcion: 'Botellas de shampoo y acondicionador para el cabello',
+    precio: 2.550,
+    imagen: '../img/shampoo.png',
+    categoría: 'Cabello',
+},
 ];
 
 // 1) Obtener el ID del carrito
@@ -93,37 +93,51 @@ const arrayDeImagenes = productos.map(img => {
 })
 console.log(arrayDeImagenes);*/
 
+let div = "";
+const crearElementos = productos.map(producto => {
 
-const crearElementos =  productos.map(producto => {
+    // 2) Crear el catálogo de c/u de los productos
 
-        // 2) Crear el catálogo de c/u de los productos
+    // a) Div que contendrá cada una de las tarjetas 
+    div = document.createElement("div");
+    // b) Crear la img
+    let img = document.createElement("img");
+    //c) Agregarle una clase
+    img.className = "tarjeta";
+    // d) Agregar el atributo src y la ruta accediendo a la propiedad imagen de cada objeto
+    img.setAttribute = ("src", producto.imagen);
+    // e) Agregar el atributo alt
+    img.setAttribute = ("alt", producto.descripcion);
+    div.append(img);
 
-        // a) Div que contendrá toda cada una de las tarjetas 
-        let div = document.createElement("div");
-        // b) Crear la img
-        let img = document.createElement("img");
-        //c) Agregarle una clase
-        img.className = "tarjeta";
-        // d) Agregar el atributo src y la ruta accediendo a la propiedad imagen de cada objeto
-        img.setAttribute = ("src", producto.imagen);
-        // e) Agregar el atributo alt
-        img.setAttribute = ("alt", producto.descripcion);
-       console.log(div.append(img));
-        
+    // 3) Crear el cuerpo de cada tarjeta 
+    let tarjetaCuerpo = document.createElement("div");
+    tarjetaCuerpo.className = "card-body";
 
-        const tarjetaCuerpo = document.createElement("div");
-        return div;
+    // 4) Crear h3 con el título de cada producto 
 
-    });
-    
+    let tituloTarjeta = document.createElement("h3");
+    tituloTarjeta.className = "card-title";
+    tituloTarjeta.innerHTML = producto.nombre;
+    // Agregar el título del producto al cuerpo de la tarjeta
+    tarjetaCuerpo.append(tituloTarjeta);
+
+    // 5) Agregar la descripción de cada producto
+    let descripcionProducto = document.createElement("p");
+    descripcionProducto.className = "card-text";
+    // Agregar la descripción de cada producto
+    descripcionProducto.innerHTML = producto.descripcion;
+    // Agregar la descripción al cuerpo de la tarjeta
+    tarjetaCuerpo.append(descripcionProducto);
+    // Al div contenedor le agrego la tarjeta con toda la info del producto
+    div.append(tarjetaCuerpo);
+    catalogo.appendChild(div);
+    return div;
+
+});
 
 
-// 3) Crear h3 con el título de cada producto
-
-
-
-
-
+// 6) Agregar el div contenedor al elemento catálogo
 
 console.log(catalogo);
 
@@ -134,14 +148,8 @@ console.log(catalogo);
 
 
 
-/* TODAVÍA NO SÉ SI USAR ÉSTA FUNCIÓN
-function agregarAtributo(elemento, atributo){
 
-    for(var key in atributo){
-elemento.setAttribute(key, atributo[key]);
-    }
-}
-agregarAtributo(img,{"src": "../img/labial.png"})*/
+
 
 
 
